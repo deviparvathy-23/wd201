e2c301ddcd0ae27232a201f08485e019f2234a4f
Devi <deviparvathys2005@gmail.com>
2025-06-19 16:42:20 +0000

    project app

--- todo-app/app.js
+++ todo-app/app.js
@@ -64,5 +64,10 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
